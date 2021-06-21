90a03c2ed28309b41c775946b43fa5f3d08b802d
Wesley Anjos <wanjos.andrade@gmail.com>
2021-06-21 11:09:32 -0300

    Organizado estrutura inicial e configurações iniciais do Firebase

---
+++ .history/services/firebase_20210621110118.ts
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+
+import 'firebase/auth';
+import 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_API_KEY,
+  authDomain: process.env.REACT_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_,
+  projectId: "letmeask-bf588",
+  storageBucket: "letmeask-bf588.appspot.com",
+  messagingSenderId: "793820074734",
+  appId: "1:793820074734:web:34b1b579b54d9b4c5599ff"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const auth = firebase.auth();
+export const database = firebase.database();

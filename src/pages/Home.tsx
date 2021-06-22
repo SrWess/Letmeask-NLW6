7e20068432ab0d364d6af3a76957738383dd75e2
Wesley Anjos <wanjos.andrade@gmail.com>
2021-06-22 15:31:59 -0300

    Criado hook de autenticação e organizado código referente ao contexto

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
 import { useHistory } from "react-router-dom";
-import { useContext } from "react";
+
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth";
 
 import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
 import "../styles/auth.scss";
-import { Button } from "../components/Button";
-import { AuthContext } from "../App";
 
 export function Home() {
   const history = useHistory();
-  const { user, signInWithGoogle } = useContext(AuthContext);
+  const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
     if (!user) {

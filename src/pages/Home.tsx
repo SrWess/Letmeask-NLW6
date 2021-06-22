a23fa58d52025a66b25c714d53241b244af68259
Wesley Anjos <wanjos.andrade@gmail.com>
2021-06-22 14:34:11 -0300

    Implementado autenticação com Firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,35 @@
-import { useHistory } from 'react-router-dom'
+import { useHistory } from "react-router-dom";
+import { auth, firebase } from "../services/firebase";
 
-import illustrationImg from '../assets/images/illustration.svg';
-import logoImg from '../assets/images/logo.svg';
-import googleIconImg from '../assets/images/google-icon.svg';
+import illustrationImg from "../assets/images/illustration.svg";
+import logoImg from "../assets/images/logo.svg";
+import googleIconImg from "../assets/images/google-icon.svg";
 
-import '../styles/auth.scss';
-import { Button } from '../components/Button';
+import "../styles/auth.scss";
+import { Button } from "../components/Button";
 
 export function Home() {
   const history = useHistory();
 
-  function navigateToNewRoom() {
-    history.push('/rooms/new');
+  function handleCreateRoom() {
+    //Autenticação do usuário
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    auth.signInWithPopup(provider).then((result) => {
+      console.log(result);
+      
+      history.push("/rooms/new");
+    });
+
   }
 
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
@@ -25,7 +37,7 @@ export function Home() {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={navigateToNewRoom}>
+          <button className="create-room" onClick={handleCreateRoom}>
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o Google
           </button>
@@ -33,17 +45,11 @@ export function Home() {
           <div className="separator">ou entre em uma sala</div>
 
           <form>
-            <input 
-              type="text"
-              placeholder="Digite o código da sala"
-            />
-            <Button type="submit">
-              Entrar na sala
-            </Button>
+            <input type="text" placeholder="Digite o código da sala" />
+            <Button type="submit">Entrar na sala</Button>
           </form>
-
         </div>
       </main>
     </div>
-  )
-} 
+  );
+}

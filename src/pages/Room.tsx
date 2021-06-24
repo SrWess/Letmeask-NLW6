17bd917cb82cea239da94efbf6c413e027e8741c
Wesley Anjos <wanjos.andrade@gmail.com>
2021-06-24 14:51:54 -0300

    Criado componente de pergunta

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,6 +3,8 @@ import { useParams } from "react-router-dom";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
+import { Question } from "../components/Question";
+
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
 
@@ -23,7 +25,7 @@ type FirebaseQuestions = Record<
   }
 >;
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -43,7 +45,7 @@ export function Room() {
 
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
   const roomId = params.id;
@@ -137,7 +139,13 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question key={question.id} content={question.content} author={question.author} />
+            );
+          })}
+        </div>
       </main>
     </div>
   );

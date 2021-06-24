d33bb7fe18a1910e2f25c4e198ee03eb071e2fea
Wesley Anjos <wanjos.andrade@gmail.com>
2021-06-24 16:13:05 -0300

    Implementado funcionalidade de like e remoção de pergunta

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,56 +1,33 @@
-import { FormEvent, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { Button } from "../components/Button";
 import { RoomCode } from "../components/RoomCode";
 import { Question } from "../components/Question";
 
-import { useAuth } from "../hooks/useAuth";
+// import { useAuth } from "../hooks/useAuth";
 import { useRoom } from "../hooks/useRoom";
-import { database } from "../services/firebase";
 
 import logoImg from "../assets/images/logo.svg";
+import deleteImg from "../assets/images/delete.svg";
 
 import "../styles/room.scss";
-
+import { database } from "../services/firebase";
 
 type RoomParams = {
   id: string;
 };
 
 export function AdminRoom() {
-  const { user } = useAuth();
-  const [newQuestion, setNewQuestion] = useState("");
-  
+  // const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
-  
+
   const { questions, title } = useRoom(roomId);
 
-  async function handleSendQuestion(event: FormEvent) {
-    event.preventDefault();
-
-    if (newQuestion.trim() === "") {
-      return;
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm("Tem certeza que deseja excluir essa pergunta?")) {
+      await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
     }
-
-    if (!user) {
-      throw new Error("You must be logged in");
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,
-      },
-      isHighlighted: false,
-      isAnswered: false,
-    };
-
-    await database.ref(`rooms/${roomId}/questions`).push(question);
-
-    setNewQuestion("");
   }
 
   return (
@@ -74,7 +51,18 @@ export function AdminRoom() {
         <div className="question-list">
           {questions.map((question) => {
             return (
-              <Question key={question.id} content={question.content} author={question.author} />
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              >
+                <button
+                  type="button"
+                  onClick={() => handleDeleteQuestion(question.id)}
+                >
+                  <img src={deleteImg} alt="Remover Pergunta" />
+                </button>
+              </Question>
             );
           })}
         </div>
